54ee42096247224f464f815d1f3d7876e74f2e96
Kboujraya <1095756998@qq.com>
2021-04-21 13:01:21 +0800

    Update diy-part3.sh

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -25,6 +25,9 @@ pushd package/community
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
 
+# Add Kenzok8's Packages
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 
