2338c8e9f0c497f561f45f29fdd72706644accea
Kboujraya <1095756998@qq.com>
2021-04-21 23:17:02 +0800

    Update diy-re.sh

--- scripts/diy-re.sh
+++ scripts/diy-re.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-popd
+p
 
 # Clone community packages to package/community
 mkdir package/community
@@ -21,41 +21,32 @@ popd
 
 # Add Lienol's Packages
 git clone --depth=1 https://github.com/Lienol/openwrt-package
-popd
 
 # Add Kenzok8's Packages
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages
-popd
 
 # Add Coolsnowwolf's Packages
 git clone --depth=1 https://github.com/coolsnowwolf/lede/tree/master/package/lean
-popd
 
 # Add luci-proto-minieap
 git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
-popd
 
 # Add luci-app-onliner
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
-popd
 
 # Add luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 rm -rf ../lean/luci-theme-argon
-popd
 
 # Add subconverter
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
-popd
 
 # Add luci-udptools
 git clone --depth=1 https://github.com/zcy85611/openwrt-luci-kcp-udp
-popd
 
 # Add OpenAppFilter
 git clone --depth=1 https://github.com/destan19/OpenAppFilter
-popd
 
 # Add driver for rtl8821cu & rtl8812au-ac
 svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
