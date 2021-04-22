1f1b7831af1e4cc4aa65fe9f1138ee9ede52a728
Kboujraya <1095756998@qq.com>
2021-04-22 12:54:47 +0800

    Update diy-re-base.sh

--- scripts/diy-re-base.sh
+++ scripts/diy-re-base.sh
@@ -12,12 +12,10 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-popd
 
 # Add feed sources
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git coolsnowwolf https://github.com/coolsnowwolf/lede' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-popd
 
 # Add luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
