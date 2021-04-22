ad6fe5c866eb956f5041d1e4f62cba76212fb13e
Kboujraya <1095756998@qq.com>
2021-04-22 22:09:21 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -13,11 +13,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# Add luci-app-ssr-plus
-pushd package/lean
-git clone --depth=1 https://github.com/fw876/helloworld
-popd
-
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
