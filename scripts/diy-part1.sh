8b11d368b88adc1dbc0563bd7f97094ccd29d7bd
Kboujraya <1095756998@qq.com>
2021-04-22 13:18:26 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -14,7 +14,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git coolsnowwolf https://github.com/coolsnowwolf/lede' feeds.conf.default
 sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
 # Add luci-theme-argon
