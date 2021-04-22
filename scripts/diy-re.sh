39eca4a913070bf79dfd1b100eed9d52ccddd3b5
Kboujraya <1095756998@qq.com>
2021-04-22 12:55:28 +0800

    Update diy-re.sh

--- scripts/diy-re.sh
+++ scripts/diy-re.sh
@@ -19,7 +19,7 @@ pushd package/community
 popd
 
 # Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
+#git clone --depth=1 https://github.com/Lienol/openwrt-package
 
 # Add Kenzok8's Packages
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages
