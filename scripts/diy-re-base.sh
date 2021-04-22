d0e11e848e119df6eeefae93481cb0cdbcd149f1
Kboujraya <1095756998@qq.com>
2021-04-22 08:35:20 +0800

    Update diy-re-base.sh

--- scripts/diy-re-base.sh
+++ scripts/diy-re-base.sh
@@ -16,7 +16,7 @@ popd
 
 # Add feed sources
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 popd
 
 # Add luci-theme-argon
