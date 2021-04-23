7a84b9930f650603f9b5d415bd5693d18ec23f2f
Kboujraya <1095756998@qq.com>
2021-04-23 23:18:39 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -17,5 +17,20 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
+# Add Kenzok8's Packages & Dependency
+git clone --depth=1 https://github.com/kenzok8/small
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+
+# Add Coolsnowwolf's Packages
+git clone --depth=1 https://github.com/coolsnowwolf/lede/tree/master/package/lean
+
 #Add packages
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-18.06 
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner # Add luci-app-onliner
+git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter # Add subconverter
+git clone --depth=1 https://github.com/zcy85611/openwrt-luci-kcp-udp # Add luci-udptools
+git clone --depth=1 https://github.com/destan19/OpenAppFilter # Add OpenAppFilter
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+rm -rf ../lean/luci-theme-argon
