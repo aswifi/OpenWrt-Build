746f1e61fed49d61a2b426cf99251af84e8b8a0e
Kboujraya <1095756998@qq.com>
2021-04-24 13:09:53 +0800

    Update diy-re.sh

--- scripts/diy-re.sh
+++ scripts/diy-re.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2021 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-re.sh
-# Description: OpenWrt DIY script re (After Update feeds)
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
 
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
-popd
 
 # Add Kenzok8's Packages & Dependency
-git clone --depth=1 https://github.com/kenzok8/small
-git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/kenzok8/small.git
+git clone https://github.com/kenzok8/openwrt-packages.git
 
 # Add Coolsnowwolf's Packages
 git clone --depth=1 https://github.com/coolsnowwolf/lede/tree/master/package/lean
 
-# Add luci-app-onliner
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+#Add other packages
+git clone https://github.com/rufengsuixing/luci-app-onliner.git # Add luci-app-onliner
+git clone https://github.com/tindy2013/openwrt-subconverter.git # Add subconverter
+git clone https://github.com/zcy85611/Openwrt-Package.git # Add luci-udptools
+git clone https://github.com/destan19/OpenAppFilter.git # Add OpenAppFilter
 
-# Add subconverter
-git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
-
-# Add luci-udptools
-git clone --depth=1 https://github.com/zcy85611/openwrt-luci-kcp-udp
-
-# Add OpenAppFilter
-git clone --depth=1 https://github.com/destan19/OpenAppFilter
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+rm -rf ../lean/luci-theme-argon
 
 # Use Lienol's https-dns-proxy package
 pushd feeds/packages/net
@@ -54,9 +52,20 @@ pushd package/kernel/mt76
 sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\/modules.d\/mt76-usb' Makefile
 popd
 
+# Mod zzz-default-settings
+#pushd package/lean/default-settings/files
+#sed -i '/http/d' zzz-default-settings
+#export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+#sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
+#popd
+
+# Modify the version number
+sed -i "s/OpenWrt /insChow build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+popd
+
 # Delete default password
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 popd
 
-# Add luci-theme-argon-18.06
-sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
+# Add luci-theme-argon-18.06_sed
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon-18.06/g' feeds/luci/collections/luci/Makefile
