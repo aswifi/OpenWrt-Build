4619027ad1f3c46123465e0dda9f186a93375e10
Kboujraya <1095756998@qq.com>
2021-04-24 10:24:38 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -14,7 +14,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
-sed -i '$a  src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git openwrt-packages https://github.com/kenzok8/openwrt-packages.git' feeds.conf.default
 
 # Add Kenzok8's Packages & Dependency
