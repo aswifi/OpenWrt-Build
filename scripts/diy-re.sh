268bd1cfca18b7cc5d2aa06656ff0aeab050449c
Kboujraya <1095756998@qq.com>
2021-05-15 11:07:29 +0800

    Update diy-re.sh

--- scripts/diy-re.sh
+++ scripts/diy-re.sh
@@ -36,7 +36,7 @@ git clone https://github.com/destan19/OpenAppFilter.git # Add OpenAppFilter
 
 # Add luci-theme-argon
 #git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
-#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 #rm -rf ../lean/luci-theme-argon
 
 # Add luci-theme-argon-18.06_sed
