b0fe8b2cb496246f7648fc598dac3713031bc0eb
Kboujraya <1095756998@qq.com>
2021-05-30 21:57:50 +0800

    Update diy-part3.sh

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -88,7 +88,7 @@ git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/lean/luci
 
 # 取消bootstrap为默认主题 & Add luci-theme-rosy
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
+#git clone https://github.com/rosywrt/luci-theme-rosy.git package/lean/luci-theme-rosy
 rm -rf package/lean/luci-theme-argon
 
 # Mod zzz-default-settings & Modify the version number & Delete default password
