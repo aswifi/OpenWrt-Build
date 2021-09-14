b4b37d960e7dea34676e8116691b507be1c7bc4e
KCYLion <1095756998@qq.com>
2021-09-14 12:35:03 +0800

    Update diy-part1-sclv2.sh

--- scripts/diy-part1-sclv2.sh
+++ scripts/diy-part1-sclv2.sh
@@ -11,19 +11,12 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git jeii https://github.com/kenzok78/jeii' feeds.conf.default
-
-# 更新queue
-git clone https://github.com/openwrt/packages
-rm -rf package/libs/libnetfilter-queue
-
-# cp命令方便本地使用
-cp -rf packages/libs/libnetfilter-queue package/libs/
 
 # 修改内核设置
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
