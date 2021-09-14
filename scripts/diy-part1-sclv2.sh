af94f6db2910918bd09087a98c68f6f96d00d05f
KCYLion <1095756998@qq.com>
2021-09-14 13:08:34 +0800

    Update diy-part1-sclv2.sh

--- scripts/diy-part1-sclv2.sh
+++ scripts/diy-part1-sclv2.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add feed sources
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
