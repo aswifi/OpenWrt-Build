2183a8108f402a04141c1a181826f6e861a3b892
KCYLion <1095756998@qq.com>
2021-10-09 22:45:54 +0800

    Update diy-part1-sclv2.sh

--- scripts/diy-part1-sclv2.sh
+++ scripts/diy-part1-sclv2.sh
@@ -18,6 +18,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git penwrt-packages https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git jeii https://github.com/kenzok78/jeii' feeds.conf.default
 #sed -i '$a src-git small-package https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages' feeds.conf.default
 
 # 修改内核设置
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/ramips/Makefile
