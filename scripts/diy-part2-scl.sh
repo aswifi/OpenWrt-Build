e74b855232bd5dcd129d7d3520b07bf4fa5b68d1
KCYLion <1095756998@qq.com>
2022-05-22 21:25:43 +0800

    Update diy-part2-scl.sh

--- scripts/diy-part2-scl.sh
+++ scripts/diy-part2-scl.sh
@@ -31,7 +31,7 @@ sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_genera
 #git clone https://github.com/coolsnowwolf/lede.git
 
 # Add Garypang13's Packages
-#git clone https://github.com/garypang13/openwrt-packages.git
+git clone https://github.com/garypang13/openwrt-packages.git package/garypang13/
 
 # Add other packages
 #git clone https://github.com/rufengsuixing/luci-app-onliner.git # Add luci-app-onliner
